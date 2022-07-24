ecc8d9f629a45601a97796940750460a680c3ce3
Sergey <serezha.sulik9@gmail.com>
2022-07-25 01:06:03 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,176 @@
+// Задание 1
+
+let str = 'Привет',
+    sum = 123,
+    num = 15.8,
+    flag = true,
+    txt = 'true';
+
+console.log (str, typeof str);
+console.log (sum, typeof sum);
+console.log (num, typeof num);
+console.log (flag, typeof flag);
+console.log (txt, typeof txt);
+
+// Задание 2
+
+let a1 = 5%3,
+    a2 = 3%5,
+    a3 = 5+'3',
+    a4 = '5'-3,
+    a5 = 75+'кг',
+    a6 = 785*653,
+    a7 = 100/25,
+    a8 = 0*0,
+    a9 = 0/2,
+    a10 = 89/0,
+    a11 = 98+2,
+    a12 = 5-98,
+    a13 = (8+56*4)/5,
+    a14 = (9-12)*7/(5+2),
+    a15 = +"123",
+    a16 = 1||0,
+    a17 = false||true,
+    a18 = true>0;
+
+console.log (a1);
+console.log (a2);
+console.log (a3);
+console.log (a4);
+console.log (a5);
+console.log (a6);
+console.log (a7);
+console.log (a8);
+console.log (a9);
+console.log (a10);
+console.log (a11);
+console.log (a12);
+console.log (a13);
+console.log (a14);
+console.log (a15);
+console.log (a16);
+console.log (a17);
+console.log (a18);
+
+console.log (a1, typeof a1);
+console.log (a2, typeof a2);
+console.log (a3, typeof a3);
+console.log (a4, typeof a4);
+console.log (a5, typeof a5);
+console.log (a6, typeof a6);
+console.log (a7, typeof a7);
+console.log (a8, typeof a8);
+console.log (a9, typeof a9);
+console.log (a10, typeof a10);
+console.log (a11, typeof a11);
+console.log (a12, typeof a12);
+console.log (a13, typeof a13);
+console.log (a14, typeof a14);
+console.log (a15, typeof a15);
+console.log (a16, typeof a16);
+console.log (a17, typeof a17);
+console.log (a18, typeof a18);
+
+// Задание 3
+
+let weight = 23,
+    height = 10,
+    SPtyam = weight * height;
+
+console.log(SPtyam);
+
+// Задание 4
+
+// S = Пи х R² х h
+
+let h = 10,
+    D = a7,
+    Pi= 3.14,
+    VCilindra = Pi*(D/2*(D/2))*h;
+
+console.log (VCilindra);
+
+// Задание 5 
+
+// S = π × r2
+
+let r = 5,
+    SKruga = Pi *(r*r);
+
+console.log (SKruga);
+
+//Задание 6
+
+//S=0.5​(a+b)⋅h
+
+let a = 5,
+    b = 7,
+    h1 = 10,
+    STrap = (a+b)/2*h1;
+
+console.log (STrap);
+
+// Задание 7
+
+let S= 2000000,
+    p = 0.1,
+    y = 5,
+    Pereplata = S*p*y;
+
+console.log (Pereplata);
+
+//Задание 8 
+
+/* Решите уравнения (найдите неизвестный x), где a = 8, b = 3:
+a+2(x-b)=16;    2(x-b)=16-a19; x-b=(16-a19)/2; x=(16-a19)/2-b
+b(x+15)=a+6x; 3x+45=8+6x  3x = 37
+x+2x+ax+bx=23780. 3x+8x+3x=23780*/
+
+let a19 = 8,
+    b1 = 3,
+    x = (16-a19)/2-b1;
+    
+
+console.log (x);
+
+let x1=37/3;
+
+console.log (x1);
+
+let x2=23780/14;
+
+console.log (x2);
+
+//Задание 9
+
+    let q = ' Бывало, спит у ног собака,\n',
+        q1 ='костер занявшийся гудит,\n',
+        q2 = 'и женщина из полумрака\n',
+        q3 = 'глазами зыбкими глядит.\n\n\n'
+        q4 = 'Потом под пихтою приляжет\n',
+        q5 = 'на куртку рыжую мою\n',
+        q6 = 'и мне, задумчивая, скажет:\n\n\n',
+        q7 = '"А ну-ка, спой!.."- и я пою.';
+
+    console.log (q,q1,q2,q3,q4,q5,q6,q7);
+
+
+// Задание 10
+
+let text = 'индо земля зашаталась под ногами-и вырос,\n',
+    text1 = 'и заревел он голосом диким...\n',
+    text2 = 'блеснула молния и ударил гром,\n',
+    text3 = 'а так какое-то чудище, страшное и мохнатое,\n',
+    text4 = 'как будто из-под земли, перед купцом:\n',
+    text5 = ' Он подошёл и сорвал аленький цветочек.\n',
+    text6 = 'зверь не зверь, человек не человек,\n',
+    text7 = 'В ту же минуту, безо всяких туч,\n';
+
+    console.log (text5,text7,text2,text,text4,text6,text3,text1)
+
+
+
+
+    
+
+
